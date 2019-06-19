7b5eaf75de65814659e6fd79c3928f18e4f76582
juanca202 <juanca.altamirano@gmail.com>
2019-06-19 15:13:06 -0500

    Improvements in navbar component

--- projects/navigation/src/lib/navigation.module.ts
+++ projects/navigation/src/lib/navigation.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule as FactorCommonModule } from 'factor-common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -19,7 +20,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     RouterModule,
     FactorCommonModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRippleModule
   ],
   exports: [
     ToolbarComponent,
--- src/app/infinite-scroll/infinite-scroll.component.ts
+++ src/app/infinite-scroll/infinite-scroll.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfiniteScrollComponent implements OnInit {
   observeStatus: boolean;
+  menu: any[] = [{
+		"name": "Invoice",
+		"locale": {
+			"es": "Facturaci\u00f3n"
+		},
+		"icon": "money",
+		"url": "\/money",
+		"_roles": ["ROLE_DEFAULT"]
+	}, {
+		"name": "Products",
+		"locale": {
+			"es": "Productos"
+		},
+		"url": "\/catalog",
+		"icon": "package",
+		"_roles": ["ROLE_DEFAULT"]
+	}, {
+		"name": "Tasks",
+		"locale": {
+			"es": "Tareas"
+		},
+		"url": "\/tasks",
+		"icon": "task",
+		"_roles": ["ROLE_DEFAULT"]
+	}, {
+		"name": "Contacts",
+		"locale": {
+			"es": "Contactos"
+		},
+		"url": "\/contacts",
+		"icon": "contacts",
+		"_roles": ["ROLE_DEFAULT"]
+	}, {
+		"name": "Help",
+		"url": "\/help",
+		"icon": "help",
+		"class": "d-none d-sm-block",
+		"_roles": ["ROLE_DEFAULT"]
+	}];
 
   constructor() { }
 
