f9b665555cbef003df5d55197de6180d77ab7240
juancaaltamirano <juanca.altamirano@gmail.com>
2020-04-02 23:50:01 -0500

    Angular 9 migration

--- dist/utils/esm2015/factor-utils.js
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZmFjdG9yLXV0aWxzLmpzIiwic291cmNlUm9vdCI6Im5nOi8vZmFjdG9yLXV0aWxzLyIsInNvdXJjZXMiOlsiZmFjdG9yLXV0aWxzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztHQUVHO0FBRUgsY0FBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljLWFwaSc7XG4iXX0=
--- dist/utils/lib/utils.module.d.ts
+++
@@ -1,9 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import * as ɵngcc0 from '@angular/core';
-export declare class UtilsModule {
-    static forRoot(configuration: any): ModuleWithProviders<UtilsModule>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UtilsModule, never, never, never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<UtilsModule>;
-}
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXRpbHMubW9kdWxlLmQudHMiLCJzb3VyY2VzIjpbInV0aWxzLm1vZHVsZS5kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztBQUNBO0FBQ0Esd0NBQXdDLGdDQUFtQjs7O0FBQzNEIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgTW9kdWxlV2l0aFByb3ZpZGVycyB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuZXhwb3J0IGRlY2xhcmUgY2xhc3MgVXRpbHNNb2R1bGUge1xuICAgIHN0YXRpYyBmb3JSb290KGNvbmZpZ3VyYXRpb246IGFueSk6IE1vZHVsZVdpdGhQcm92aWRlcnM7XG59XG4iXX0=
